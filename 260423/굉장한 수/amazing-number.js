76b0dbecfa64b413bc4ac96e0c653e843a685589
Jimin Kim <101644134+jimin-ni@users.noreply.github.com>
2026-04-23 03:55:57 +0900

    Title: 굉장한 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260423/굉장한 수/amazing-number.js
+++ 260423/굉장한 수/amazing-number.js
@@ -6,7 +6,7 @@ let N = Number(inputNum)
 if(N%2==1 && N%3==0) {
     console.log('true')
 } else if (N%2==0 && N%5==0) {
-    console.log('ture')
+    console.log('true')
 } else {
     console.log('false')
 }
