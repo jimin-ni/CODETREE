3174b0f9b93eadf37b703f2470bd98dceb176157
Jimin Kim <101644134+jimin-ni@users.noreply.github.com>
2026-04-23 03:54:18 +0900

    Title: 굉장한 수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260423/굉장한 수/amazing-number.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+let inputNum = fs.readFileSync(0).toString().trim();
+
+let N = Number(inputNum)
+
+if(N%2==1 && N%3==0) {
+    console.log('true')
+} else if (N%2==0 && N%5==0) {
+    console.log('ture')
+} else {
+    console.log('false')
+}
