a280239f2106caa4f05f8495c5236260db678ba6
Jimin Kim <101644134+jimin-ni@users.noreply.github.com>
2026-04-25 20:57:03 +0900

    Title: 1부터의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260425/1부터의 합/sum-from-the-one.js
+++ 260425/1부터의 합/sum-from-the-one.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 let N = fs.readFileSync(0).toString().trim();
-let a = 1
+let sum = 0;
 
-for ( i = 0; i <= 100; i++){
-    a = a + 1
-    if(a >= Number(N)) {
-        console.log(i)
-        break
+for ( i = 1; i <= 100; i++){
+    sum = sum + i;
+    if(sum >= Number(N)) {
+        console.log(i);
+        break;
     }
 }
