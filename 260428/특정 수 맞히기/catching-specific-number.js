6c1bd337db6cdebf9223c95c4185f1552bca9717
Jimin Kim <101644134+jimin-ni@users.noreply.github.com>
2026-04-28 14:57:59 +0900

    Title: 특정 수 맞히기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260428/특정 수 맞히기/catching-specific-number.js
+++ 260428/특정 수 맞히기/catching-specific-number.js
@@ -3,13 +3,15 @@ let inputNum = fs.readFileSync(0).toString().trim().split('\n');
 let idx = 0;
 
 while (true) {
-    num = inputNum[idx];
-    if (Number(num) < 25){
+    num = Number(inputNum[idx]);
+
+    if (num < 25){
         console.log('Higher')
-    }  else if (Number(num > 25)) {
+    }  else if (num > 25) {
         console.log('Lower')
-    } else if (Number(num) == 25) {
+    } else if (num == 25) {
         console.log('Good')
+        break;
     }
     idx++;
 }
