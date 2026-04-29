2f38db1341a16a71c8789a6635e2925bcf66b125
Jimin Kim <101644134+jimin-ni@users.noreply.github.com>
2026-04-29 23:04:11 +0900

    Title: 짝수의 합 n번 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260429/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.js
+++ 260429/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.js
@@ -2,10 +2,13 @@ const fs = require('fs');
 let arr = fs.readFileSync(0).toString().trim().split(/\s+/);
 
 let N = Number(arr[0]);
+let idx = 1
 
-for (let i = 1; i <= N+2; i = i + 2) {
-    let a = Number(arr[i]);
-    let b = Number(arr[i + 1]);
+for (let i = 1; i <= N; i++) {
+    let a = Number(arr[idx]);
+    let b = Number(arr[idx + 1]);
+    idx = idx + 2
+
     let str = 0
 
     for (let j = a; j <= b; j++) {
