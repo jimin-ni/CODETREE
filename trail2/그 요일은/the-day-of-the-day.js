3cf58d1eae4da3e7caf10dcb68cb8589484fb7ae
Jimin Kim <101644134+jimin-ni@users.noreply.github.com>
2026-06-17 22:37:37 +0900

    [Passed] 그 요일은 (66ms, 10MB)

---
+++ trail2/그 요일은/the-day-of-the-day.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+const A = input[1];
+
+// Please Write your code here.
+
+const num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+// 1월 1일부터 각각 며칠 차이가 나는지 구하기
+function totalDaysFromJan1(m, d) {
+    let totalDays = 0
+    for (let i = 0; i < m; i++) {
+        totalDays += num_of_days[i]
+    }
+    totalDays = totalDays + d
+    return totalDays
+}
+
+let days1 = totalDaysFromJan1(m1, d1);
+let days2 = totalDaysFromJan1(m2, d2);
+
+let leftDays = days2 - days1 + 1;
+
+let numTotal = parseInt(leftDays / 7)
+
+let weekInstans = ((leftDays % 7) + 7) % 7
+
+for (let i = 0; i < weekInstans; i++) {
+    if (week[i] === A) {
+        numTotal = numTotal + 1
+    }
+}
+
+
+
+console.log(numTotal)
+
+
+
+/*
+if (m1 == m2) {
+    left_days = d2 - d1
+} else if (m1 < m2) {
+    // m1 < m2 m1달의 남은 일 수 + m1과 m2 사이의 달 + d2 날 을 더하기 
+    for (let i = m1 + 1; i < m2; i++) {
+        left_days = left_days + num_of_days[i]
+    }
+    left_days = left_days + (num_of_days[m1] - d1) + d2
+}
+
+
+// 7로 left_days를 나눈 몫이 횟수에 포함됨
+let sum = 0
+sum = Math.floor(left_days / 7)
+
+// 나눈 나머지
+let num = 0
+num = (left_days % 7)
+for (let i = 1; i <= num; i++) {
+    if (week[i] == A) {
+        sum = sum + 1
+    }
+}
+
+console.log(sum)
+*/
