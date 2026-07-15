36a43cc4cbb674ac6848b17bde70f89531fe266b
Jimin Kim <101644134+jimin-ni@users.noreply.github.com>
2026-07-16 00:25:23 +0900

    [Passed] X 달리기 (63ms, 10MB)

--- trail2/X 달리기/x-run.js
+++ trail2/X 달리기/x-run.js
@@ -1,24 +1,23 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 
-const x = Number(input[0]);
+let x = Number(input[0]);
 
-function solve(X) {
-  let v = 1;
-  while (v * v <= X) {
-    v++;
+let time = 0
+let currunt_v = 1
+
+while (x > 0) {
+  x = x - currunt_v
+  time = time + 1
+
+  if (x <= 0) {
+    break;
   }
-  v--;
 
-  const remaining = X - v * v;
+  x = x - currunt_v
+  time = time + 1
 
-  if (remaining === 0) {
-    return 2 * v - 1;
-  } else if (remaining <= v) {
-    return 2 * v;
-  } else {
-    return 2 * v + 1;
-  }
+  currunt_v = currunt_v + 1
 }
 
-console.log(solve(x));
+console.log(time)
